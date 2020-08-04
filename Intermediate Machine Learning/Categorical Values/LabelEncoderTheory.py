8ee2a7eed07834289f9db00bda48b17750d45ded
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-08-04 13:33:04 +0530

    Create LabelEncoderTheory.py

---
+++ Intermediate Machine Learning/Categorical Values/LabelEncoderTheory.py
@@ -0,0 +1,14 @@
+from sklearn.preprocessing import LabelEncoder
+
+# Make copy to avoid changing original data 
+label_X_train = X_train.copy()
+label_X_valid = X_valid.copy()
+
+# Apply label encoder to each column with categorical data
+label_encoder = LabelEncoder()
+for col in object_cols:
+    label_X_train[col] = label_encoder.fit_transform(X_train[col])
+    label_X_valid[col] = label_encoder.transform(X_valid[col])
+
+print("MAE from Approach 2 (Label Encoding):") 
+print(score_dataset(label_X_train, label_X_valid, y_train, y_valid))
